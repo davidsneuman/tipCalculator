cbee8d4ae2431c783bbe888c758aa507d885a7cf
David Neuman <davidsneuman@gmail.com>
2020-12-31 10:45:10 -0500

    Initial Commit

---
+++ tipCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tipCalculator
+//
+//  Created by David Neuman on 12/31/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
