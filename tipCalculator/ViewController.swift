b284d8c84f155c092c862a3537acfe9919cbb213
David Neuman <davidsneuman@gmail.com>
2020-12-31 19:37:24 -0500

    cleaned up code redundancy.

--- tipCalculator/ViewController.swift
+++ tipCalculator/ViewController.swift
@@ -35,41 +35,10 @@ class ViewController: UIViewController {
         view.endEditing(true)
     }
     
-    //split tip
 
 
-    @IBAction func splitCounterIncrement(_ sender: UIStepper) {
-        
-        splitLabel.text = Int(sender.value).description
-        // Get the bill amount
-        let bill = Double(billField.text!) ?? 0
-        
-        // Calculate the tip and total
-        let tipPercentages = [0.15, 0.18, 0.20, 0.25]
-        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
-        
-        let numPeople = Double(splitLabel.text!) ?? 1
-        
 
-        let total = bill + tip
-        
-        let totalEach = (bill + tip) / numPeople
-        
-        if numPeople > 1{
-            youEachOweLabel.text = "You each owe"
-            totalEachLabel.text = String(format: "$%.2f",totalEach)
-            
-        } else{
-            youEachOweLabel.text = ""
-            totalEachLabel.text = ""
-        }
-        
-        
-        // Update the tip and total labels
-        tipLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
-        
-    }
+
     
     @IBAction func calculateTip(_ sender: Any) {
         // Get the bill amount
@@ -100,6 +69,14 @@ class ViewController: UIViewController {
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
         
+    }
+    
+    //split tip
+    @IBAction func splitCounterIncrement(_ sender: UIStepper) {
+        
+        splitLabel.text = Int(sender.value).description
+
+        calculateTip(self)
     }
 
     
